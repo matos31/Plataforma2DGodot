37107a3aaadca83c52722feac17289a11136a1d6
Rafael Forbeck <rafaelforbeck@gmail.com>
2025-03-06 21:41:13 -0300

    cria tilesets

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 30.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,13 +16,13 @@ func _physics_process(delta: float) -> void:
 			anim.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
